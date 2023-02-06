71fb81cecd10fca54a7f9344fb45558de7da451e
kalayu-ftsum <ftsumkalayu12@gmail.com>
2023-02-06 20:08:09 +0100

    added folder structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './components/Header/header';
+
+class App extends Component {
+  render() {
+    return (
+    <>
+     <Header />
+     </>
+    );
+  }
+}
+
+export default App;
